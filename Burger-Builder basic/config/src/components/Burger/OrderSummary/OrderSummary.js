8e7df118b45536513d20fed755569050fb666dd4
shivamnarware <51173814+shivamnarware@users.noreply.github.com>
2020-08-12 01:29:13 +0530

    Add files via upload

---
+++ Burger-Builder basic/config/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
+
+const orderSummary = (props) => {
+    const ingredientsummary = Object.keys(props.ingredients).
+        map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>
+                :{props.ingredients[igKey]}
+                </li>)
+        });
+
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredient:</p>
+            <ul>
+                {ingredientsummary}
+            </ul>
+            <p>Continue to checkOut</p>
+            <p><strong>Total price:{props.price}</strong></p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
+        </Aux>
+    );
+};
+export default orderSummary;
---
+++ Burger-Builder basic/config/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+const sideDrawer = (props) => {
+    let attachedclasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedclasses = [classes.SideDrawer, classes.Open];
+    }
+
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedclasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    )
+};
+
+export default sideDrawer;
